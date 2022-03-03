06b264e5fec0589a2a82f8cb09f2a1afb079fc59
magziel <magszczepkowska@gmail.com>
2022-03-03 12:21:36 +0100

    SID-1 - Dodanie @BeforeAll

--- src/test/java/KontaktSeleniumTest.java
+++ src/test/java/KontaktSeleniumTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,8 +9,12 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class KontaktSeleniumTest {
 
-    public WebDriver getDriver() {
+    @BeforeAll
+    public static void  setup(){
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+    }
+
+    public WebDriver getDriver() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to("http://192.168.0.167:4200/kontakt");
